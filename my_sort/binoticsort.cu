c19a603fdcf99f85fe2e85c459516bb2132e3d7e
whiteget333 <zhanghuateng2000@gmail.com>
2024-11-20 23:09:21 +0800

    my_sort

--- my_sort/binoticsort.cu
+++ my_sort/binoticsort.cu
@@ -2,22 +2,58 @@
 #include <cstdio>
 #include <iostream>
 #include "cuda_runtime_api.h"
-#include "E:\cproject\cuda_c\CUDA_C\include\freshman.h"
+#include "freshman.h"
 #include <algorithm>
 
 #define tile_size 256u
 #define log_tile_size 8u
-
+#define DBL_MAX 1.79769e+308
 template<typename type>__device__ void swap(type& v1, type& v2)
 {
 	type t = v1;
 	v1 = v2;
 	v2 = t;
 }
+template<typename type>__device__ unsigned int lower_bound(const type* data, unsigned int n, const type& value)
+{
+ unsigned int len = n, half;
+ const type* start = data, * mid;
+ while (len)
+ {
+  half = len >> 1;
+  mid = data + half;
+  if (*mid < value)
+  {
+   data = mid + 1;
+   len = len - half - 1;
+  }
+  else
+   len = half;
+ }
+ return data - start;
+}
+template<typename type>__device__ unsigned int upper_bound(const type* data, unsigned int n, const type& value)
+{
+ unsigned int len = n, half;
+ const type* start = data, *mid;
+ while (len)
+ {
+  half = len >> 1;
+  mid = data + half;
+  if (value < *mid)
+   len = half;
+  else
+  {
+   data = mid + 1;
+   len = len - half - 1;
+  }
+ }
+ return data - start;
+}
 
 __global__ void binoticsort_gpu(float* data, int n)
 {
-	__shared__  double buffer[tile_size << 1];
+	__shared__  float buffer[tile_size << 1];
 
 	unsigned int d = blockIdx.x << (log_tile_size + 1), i = threadIdx.x;
 	data += d; n -= d;
@@ -26,35 +62,103 @@ __global__ void binoticsort_gpu(float* data, int n)
 	__syncthreads();
 
 	unsigned int s, t;
-	for (unsigned int k = 2; k <= blockDim.x; k <<= 1)
+	for (unsigned int k = 2; k <= blockDim.x * 2; k <<= 1)
 		for (unsigned int p = k >> 1; p; p >>= 1)
 		{
 			s = (i & -p) << 1 | i & p - 1;
 			t = s | p;
 
-			if (s & k ? buffer[s] > buffer[t] : buffer[s] < buffer[t])
+			if (s & k ? buffer[s] < buffer[t] : buffer[s] > buffer[t])
 				swap(buffer[s], buffer[t]);
 			__syncthreads();
 		}
-	for (unsigned int p = blockDim.x; p; p >>= 1)
-	{
-		s = (i & -p) << 1 | i & p - 1;
-		t = s | p;
-		if (buffer[s] > buffer[t])
-			swap(buffer[s], buffer[t]);
-		__syncthreads();
-	}
+
 
 	if (i < n)
 		data[i] = buffer[i];
 	if (i + blockDim.x < n)
 		data[i + blockDim.x] = buffer[i + blockDim.x];
 }
-
-void binoticsort(float* data, const int n)
+// merge directly
+__global__ void mergedirect_gpu(const float* data, unsigned int n, unsigned int s, float* result)
 {
-    binoticsort_gpu<<<(n-1) / tile_size + 1, tile_size>>>(data, n);
+ unsigned int d1 = (blockIdx.z * gridDim.y + blockIdx.y) * s << 1, d2 = d1 + s, i = d1 + blockIdx.x * blockDim.x + threadIdx.x;
+ if (i < n)
+  result[d2 < n ? i + lower_bound(data + d2, min(s, n - d2), data[i]) : i] = data[i];
+ if (s + i < n)
+  result[i + upper_bound(data + d1, s, data[s + i])] = data[s + i];
+}
+float* sortdirect_gpu(float* data, unsigned int n)
+{
+ float* tmp;
+ cudaMalloc(&tmp, sizeof(float) * n);
+ unsigned int b = n + (tile_size << 1) - 1 >> log_tile_size + 1, s = tile_size << 1;
+ binoticsort_gpu<<<b, s >> 1>>>(data, n);
+ for (b = b + 1 >> 1; s < n; s <<= 1, b = b + 1 >> 1)
+ {
+  mergedirect_gpu<<<dim3(s >> log_tile_size, std::min(b, 32768u), b + 32767 >> 15), tile_size>>>(data, n, s, tmp);
+  std::swap(data, tmp);
+ }
+ cudaDeviceSynchronize();
+ cudaFree(tmp);
+ 
+ return data;
+}
 
+__global__ void _bitonic_sort(float* data, unsigned stride, unsigned inner_stride) {
+    unsigned tid = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned s = (tid & -inner_stride) << 1 | tid & inner_stride - 1;
+	unsigned t = s | inner_stride;
+
+    if (s & stride ? data[s] < data[t] : data[s] > data[t])
+		swap(data[s], data[t]);
+    
+}
+
+void float_sort(float *arr, int len) {
+    // 首先检查长度是否为 2 的幂
+    unsigned twoUpper = 1;
+    for (;twoUpper < len; twoUpper <<= 1) {
+        if (twoUpper == len) {
+            break;
+        }
+    }
+
+    // 如果是 host 指针，返回
+    cudaPointerAttributes attrs;
+    cudaPointerGetAttributes(&attrs, arr);
+    if (attrs.type != cudaMemoryTypeDevice) {
+        return;
+    }
+
+    unsigned input_arr_len = len;
+    // if (twoUpper == len) {
+    //     input_arr_len = len;
+    //     d_input_arr = arr;
+    // } else {
+    //     // 需要 padding
+    //     input_arr_len = twoUpper;
+    //     cudaMalloc(&d_input_arr, sizeof(float) * input_arr_len);
+    //     // 然后初始化
+    //     cudaMemcpy(d_input_arr, arr, sizeof(float) * len, cudaMemcpyHostToDevice);
+    //     cudaMemset(d_input_arr + len, 0x7f, sizeof(float) * (input_arr_len - len));
+    // }
+
+    dim3 grid_dim((input_arr_len / 256 == 0)? 1 : input_arr_len / 256);
+    dim3 block_dim((input_arr_len / 256 == 0)? input_arr_len : 256);
+    
+    // 排序过程(重点)
+    for (unsigned stride = 2; stride <= input_arr_len; stride <<= 1) {
+        for (unsigned inner_stride = stride>>1; inner_stride > 0; inner_stride >>= 1) {
+            _bitonic_sort<<<grid_dim, block_dim>>>(arr, stride, inner_stride);
+        }
+    }
+
+    // 如果 padding 过，则此处还原
+    // if (twoUpper != len) {
+    //     cudaMemcpy(arr, d_input_arr, sizeof(float) * len, cudaMemcpyDeviceToDevice);
+    //     cudaFree(d_input_arr);
+    // }
 }
 
 void printf_data(float* data, const int n)
@@ -71,50 +175,66 @@ bool cmp(float i, float j)
     return i<j;
 }
 
+
+
 int main()
 {
-    const int num_items = 2>>8;
+    const int num_items = 2<<24;
     double iStart, iElaps;
     float *h_data = NULL;
-    float *d_data = NULL;
+    float *d_data_1 = NULL;
+    float *d_data_2 = NULL;
     //int *h_res = NULL;
-    float *d_res = NULL;
-    
+    float *d_res_1 = NULL;
+    float *d_res_2 = NULL;
+     
     // 分配内存
     h_data = (float*)malloc(num_items * sizeof(float));
-    d_res = (float*)malloc(num_items * sizeof(float));
+    d_res_1 = (float*)malloc(num_items * sizeof(float));
+    d_res_2 = (float*)malloc(num_items * sizeof(float));
     //h_res =  = (int*)malloc(num_items * sizeof(int));
-    CHECK(cudaMalloc( (void**)&d_data, num_items * sizeof(float)))
+    CHECK(cudaMalloc( (void**)&d_data_1, num_items * sizeof(float)))
+    CHECK(cudaMalloc( (void**)&d_data_2, num_items * sizeof(float)))
 
     // 初始化数据
-    initialData_float(h_data, num_items);
-    printf_data(h_data, num_items);
+    initialData(h_data, num_items);
+    // printf_data(h_data, num_items);
     // 数据传输
-    CHECK(cudaMemcpy(d_data, h_data, num_items * sizeof(float), cudaMemcpyHostToDevice))
-    
+    CHECK(cudaMemcpy(d_data_1, h_data, num_items * sizeof(float), cudaMemcpyHostToDevice))
+    CHECK(cudaMemcpy(d_data_2, h_data, num_items * sizeof(float), cudaMemcpyHostToDevice))
+
     // GPU sort
     iStart=cpuSecond();
-    binoticsort(d_data, num_items);
+    d_data_1 = sortdirect_gpu(d_data_1, num_items);
     // 数据传输 
-    CHECK(cudaMemcpy(d_res, d_data, num_items * sizeof(float), cudaMemcpyDeviceToHost))
+    CHECK(cudaMemcpy(d_res_1, d_data_1, num_items * sizeof(float), cudaMemcpyDeviceToHost))
     iElaps=cpuSecond()-iStart;
     printf("Device sort Time elapsed %f sec\n",iElaps);
 
+    // GPU sort 2
+    // iStart=cpuSecond();
+    // float_sort(d_data_2, num_items);
+    // // 数据传输 
+    // CHECK(cudaMemcpy(d_res_2, d_data_2, num_items * sizeof(float), cudaMemcpyDeviceToHost))
+    // iElaps=cpuSecond()-iStart;
+    // printf("Device sort Time elapsed %f sec\n",iElaps);
+
+    // printf_data(d_res, num_items);
     // CPU sort
     iStart=cpuSecond();
     std::sort(h_data, h_data + num_items,cmp);
     iElaps=cpuSecond()-iStart;
     printf("Host sort Time elapsed %f sec\n",iElaps);
 
-    
-
-    
     // check result
-    checkResult(d_res, h_data, num_items);
+    checkResult(d_res_2, h_data, num_items);
+    checkResult(d_res_1, h_data, num_items);
 
     free(h_data);
-    free(d_res);
-    CHECK(cudaFree(d_data));
+    free(d_res_1);
+    free(d_res_2);
+    CHECK(cudaFree(d_data_1));
+    CHECK(cudaFree(d_data_2));
 
     return 0;
 }
---
+++ my_sort/test.cu
@@ -0,0 +1,175 @@
+#include <cuda.h>
+#include "cuda_runtime_api.h"
+#include <device_launch_parameters.h>
+#include <iostream>
+#include <algorithm>
+#include <stdio.h>
+#include <cstdio>
+#include "freshman.h"
+
+
+#define tile_size 256u
+#define log_tile_size 8u
+#define DBL_MAX 1.79769e+308
+
+
+ template<typename type>__device__ void swap(type& v1, type& v2)
+{
+	type t = v1;
+	v1 = v2;
+	v2 = t;
+}
+template<typename type>__device__ unsigned int lower_bound(const type* data, unsigned int n, const type& value)
+{
+ unsigned int len = n, half;
+ const type* start = data, * mid;
+ while (len)
+ {
+  half = len >> 1;
+  mid = data + half;
+  if (*mid < value)
+  {
+   data = mid + 1;
+   len = len - half - 1;
+  }
+  else
+   len = half;
+ }
+ return data - start;
+}
+template<typename type>__device__ unsigned int upper_bound(const type* data, unsigned int n, const type& value)
+{
+ unsigned int len = n, half;
+ const type* start = data, *mid;
+ while (len)
+ {
+  half = len >> 1;
+  mid = data + half;
+  if (value < *mid)
+   len = half;
+  else
+  {
+   data = mid + 1;
+   len = len - half - 1;
+  }
+ }
+ return data - start;
+}
+
+__global__ void binoticsort_gpu(double* data, int n)
+{
+	__shared__  double buffer[tile_size << 1];
+
+	unsigned int d = blockIdx.x << (log_tile_size + 1), i = threadIdx.x;
+	data += d; n -= d;
+	buffer[i] = i < n ? data[i] : DBL_MAX;
+	buffer[i + blockDim.x] = i + blockDim.x < n ? data[i + blockDim.x] : DBL_MAX;
+	__syncthreads();
+
+	unsigned int s, t;
+	for (unsigned int k = 2; k <= blockDim.x * 2; k <<= 1)
+		for (unsigned int p = k >> 1; p; p >>= 1)
+		{
+			s = (i & -p) << 1 | i & p - 1;
+			t = s | p;
+
+			if (s & k ? buffer[s] < buffer[t] : buffer[s] > buffer[t])
+				swap(buffer[s], buffer[t]);
+			__syncthreads();
+		}
+
+
+	if (i < n)
+		data[i] = buffer[i];
+	if (i + blockDim.x < n)
+		data[i + blockDim.x] = buffer[i + blockDim.x];
+}
+// merge directly
+__global__ void mergedirect_gpu(const double* data, unsigned int n, unsigned int s, double* result)
+{
+ unsigned int d1 = (blockIdx.z * gridDim.y + blockIdx.y) * s << 1, d2 = d1 + s, i = d1 + blockIdx.x * blockDim.x + threadIdx.x;
+ if (i < n)
+  result[d2 < n ? i + lower_bound(data + d2, min(s, n - d2), data[i]) : i] = data[i];
+ if (s + i < n)
+  result[i + upper_bound(data + d1, s, data[s + i])] = data[s + i];
+}
+double* sortdirect_gpu(double* data, unsigned int n)
+{
+ double* tmp;
+ cudaMalloc(&tmp, sizeof(double) * n);
+ unsigned int b = n + (tile_size << 1) - 1 >> log_tile_size + 1, s = tile_size << 1;
+ binoticsort_gpu<<<b, s >> 1>>>(data, n);
+ for (b = b + 1 >> 1; s < n; s <<= 1, b = b + 1 >> 1)
+ {
+  mergedirect_gpu<<<dim3(s >> log_tile_size, std::min(b, 32768u), b + 32767 >> 15), tile_size>>>(data, n, s, tmp);
+  std::swap(data, tmp);
+ }
+ cudaDeviceSynchronize();
+ cudaFree(tmp);
+ 
+ return data;
+}
+
+int main(int argc, char** argv)
+{
+ cudaSetDevice(0);
+ cudaFree(0);
+ unsigned int test[] = { 1025u, 65537u, 1048577u, 16777217u, 134217729u, 1073741825u };
+ const unsigned int runtime = 3;
+ 
+ double iStart, iElaps;
+ double elasped[3] = { 0 };
+ 
+ for (unsigned int t = 0; t < sizeof(test) / sizeof(unsigned int); ++t)
+ {
+  unsigned int n = test[t];
+  double* original = new double[n];
+  double* data = new double[n];
+ 
+  elasped[0] = elasped[1] = elasped[2] = 0;
+  srand(time(nullptr));
+  for (int i = 0; i < runtime; ++i)
+  {
+   for (unsigned int i = 0; i < n; ++i)
+    original[i] = data[i] = (rand() * 2.0 / RAND_MAX - 1.0) * rand();
+ 
+   // cpu sort 1
+iStart=cpuSecond();
+   qsort(data, n, sizeof(double), [](const void* left, const void* right)->int {double tmp = *(double*)left - *(double*)right; if (tmp > 0) return 1; else if (tmp < 0) return -1; else return 0; });
+    iElaps=cpuSecond()-iStart;
+   elasped[0] += iElaps;
+ 
+   // cpu sort 2
+   memcpy(data, original, sizeof(double) * n);
+iStart=cpuSecond();
+   std::sort(data, data + n);
+    iElaps=cpuSecond()-iStart;
+   elasped[1] += iElaps;
+ 
+ 
+   // gpu sort
+   double* pd_data;
+   cudaMalloc(&pd_data, sizeof(double) * n);
+   cudaMemcpy(pd_data, original, sizeof(double) * n, cudaMemcpyHostToDevice);
+
+iStart=cpuSecond();
+   pd_data = sortdirect_gpu(pd_data, n);
+    iElaps=cpuSecond()-iStart;
+   elasped[2] += iElaps;
+
+   cudaMemcpy(data, pd_data, sizeof(double) * n, cudaMemcpyDeviceToHost);
+   cudaFree(pd_data);
+  }
+ 
+  printf("data number: %u\n", n);
+  printf("cpu merge sort cost %.5f ms\n", elasped[0] / 1e3 / runtime);
+  printf("cpu quick sort cost %.5f ms\n", elasped[1] / 1e3 / runtime);
+  printf("gpu sort cost %.5f ms\n", elasped[2] / 1e3 / runtime);
+  printf("-------------------------------------------\n");
+ 
+  delete[] data;
+  delete[] original;
+ }
+ 
+ return 0;
+}
